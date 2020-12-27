40415de2620743aeaac6f854fa395605f6cab039
Jones S. Croppper <callowcreation@gmail.com>
2020-12-27 12:36:31 -0800

    voice browser started seems like it can not be a browser source to use the mic

--- functions/index.js
+++ functions/index.js
@@ -19,7 +19,7 @@ app.get('/camera-glow', (req, res) => {
 	const url = req.url.split('?')[1];
 	const req_data = qs.parse(url);
 	const { channel, command, color1, color2, color3 } = req_data;
-	if(!channel) {
+	if (!channel) {
 		return res.status(500).send('channel param required');
 	}
 	res.render('camera-glow/index', {
@@ -31,5 +31,25 @@ app.get('/camera-glow', (req, res) => {
 	});
 });
 
+
+app.get('/voice', (req, res) => {
+
+	const qs = require('querystring');
+	const url = req.url.split('?')[1];
+	const req_data = qs.parse(url);
+	const { channel, trigger } = req_data;
+	if (!channel) {
+		return res.status(500).send('channel param required');
+	}
+	res.render('voice/index', {
+		channel: channel,
+		trigger: trigger || 'command'
+	});
+});
+
+app.post('/voice', (req, res) => {
+	console.log(req.body);
+	res.json({ channel: req.body.channel, id: '42' });
+});
 
 exports.app = functions.https.onRequest(app);
